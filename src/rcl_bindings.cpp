d76a29bbc68b808634549bb2e042ae5d6909be9b
Kenny Yuan <kaining.yuan@intel.com>
2017-06-22 13:53:59 +0800

    Fixing lint issues

--- src/rcl_bindings.cpp
+++ src/rcl_bindings.cpp
@@ -19,6 +19,8 @@
 #include <rcl/node.h>
 #include <rcl/rcl.h>
 
+#include <string>
+
 #include "handle_manager.hpp"
 #include "rcl_handle.hpp"
 #include "shadow_node.hpp"
@@ -73,7 +75,8 @@ NAN_METHOD(CreateNode) {
 
   *node = rcl_get_zero_initialized_node();
   rcl_node_options_t options = rcl_node_get_default_options();
-  if (rcl_node_init(node, node_name.c_str(), name_space.c_str(), &options) != RCL_RET_OK) {
+  if (rcl_node_init(node, node_name.c_str(),
+      name_space.c_str(), &options) != RCL_RET_OK) {
     Nan::ThrowError(rcl_get_error_string_safe());
     return;
   }
@@ -327,7 +330,7 @@ NAN_METHOD(CreateSubscription) {
   std::string topic(*Nan::Utf8String(info[4]->ToString()));
 
   rcl_subscription_t * subscription =
-    (rcl_subscription_t*)malloc(sizeof(rcl_subscription_t));
+    reinterpret_cast<rcl_subscription_t*>(malloc(sizeof(rcl_subscription_t)));
   *subscription = rcl_get_zero_initialized_subscription();
 
   rcl_subscription_options_t subscription_ops =
